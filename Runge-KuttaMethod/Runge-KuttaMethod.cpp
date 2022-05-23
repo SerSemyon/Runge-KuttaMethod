6b302ea38d1b860308ebfa1c367d4d616e9013be
SYSerov <SYSerov@IVM-14-1112-06C>
2022-05-23 16:09:54 +0300

    Убраны старые варианты

--- Runge-KuttaMethod/Runge-KuttaMethod.cpp
+++ Runge-KuttaMethod/Runge-KuttaMethod.cpp
@@ -33,7 +33,7 @@ double Z(double t, double* y) {
 double X(double t, double* y) {
     return y[0] * cos(y[1]);
 }
-void RungeKutta(double (*f1)(double t, double y1, double y2, double y3, double y4), double (*f2)(double t, double y1, double y2, double y3, double y4), double (*f3)(double t, double y1, double y2, double y3, double y4), double (*f4)(double t, double y1, double y2, double y3, double y4), double* y1, double* y2, double* y3, double* y4, double h, double n, double* t, double y1_0, double y2_0, double y3_0, double y4_0) {
+void RungeKutta(double (*f1)(double t, double y1, double y2, double y3, double y4), double (*f2)(double t, double y1, double y2, double y3, double y4), double (*f3)(double t, double y1, double y2, double y3, double y4), double (*f4)(double t, double y1, double y2, double y3, double y4), double* y1, double* y2, double* y3, double* y4, double h, int n, double* t, double y1_0, double y2_0, double y3_0, double y4_0) {
     y1[0] = y1_0;
     y2[0] = y2_0;
     y3[0] = y3_0;
@@ -58,50 +58,7 @@ void RungeKutta(double (*f1)(double t, double y1, double y2, double y3, double y
     }
 }
 
-//void RungeKutta(double (*f1)(double t, double y1, double y2), double(*f2)(double t, double y1, double y2), double* y1, double* y2, double h, double n, double* t, double y1_0, double y2_0) {
-//    y1[0] = y1_0;
-//    y2[0] = y2_0;
-//    double k11, k12, k21, k22;
-//    for (int i = 0; i < n; i++) {
-//        k11 = f1(t[i], y1[i], y2[i]);
-//        k12 = f2(t[i], y1[i], y2[i]);
-//
-//        k21 = f1(t[i] + h, y1[i] + h * k11, y2[i] + h * k12);
-//        k22 = f2(t[i] + h, y1[i] + h * k11, y2[i] + h * k12);
-//
-//        y1[i + 1] = y1[i] + h * (k11 + k21) / 2;
-//        y2[i + 1] = y2[i] + h * (k12 + k22) / 2;
-//    }
-//}
-
-
-
-void RungeKutta(double (**f)(double t, double* y), double** y, double h, double n, double* t, double* y0) {
-    int count = 2;
-    for (int i = 0; i < count; i++) {
-        y[0][i] = y0[i];
-    }
-    double* k1 = new double[count];
-    double* k2 = new double[count];
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < count; j++) {
-            k1[j] = f[j](t[i], y[i]);
-        }
-        double* newY = new double[count];
-        for (int j = 0; j < count; j++) {
-            newY[j] = y[i][j] + h * k1[j];
-        }
-        for (int j = 0; j < count; j++) {
-            k2[j] = f[j](t[i] + h, newY);
-        }
-        for (int j = 0; j < count; j++) {
-            y[i + 1][j] = y[i][j] + h * (k1[j] + k2[j]) / 2;
-        }
-    }
-}
-
-
-void RungeKutta(int count, double (**f)(double t, double* y), double** y, double h, double n, double* t, double* y0) {
+void RungeKutta(int count, double (**f)(double t, double* y), double** y, double h, int n, double* t, double* y0) {
     for (int i = 0; i < count; i++) {
         y[0][i] = y0[i];
     }
@@ -129,7 +86,6 @@ int main()
     
     int n = 1000;
     double h = 2.0 / n;
-    /*
     double* y1 = new double[n]();
     double* y2 = new double[n]();
     double* t = new double[n];
@@ -147,15 +103,12 @@ int main()
     y0[0] = testY1(t[0]);
     y0[1] = testY2(t[0]);
     std::cout << y0[0] << " " << y0[1] << "\n";
-    //RungeKutta(TestF1, TestF2, y, h, n, t, testY1(t[0]), testY2(t[0]));
     //RungeKutta(f, y, h, n, t, y0);
     RungeKutta(2, f, y, h, n, t, y0);
     for (int i = 0; i < n; i++) {
         double testy1 = testY1(t[i]);
         double testy2 = testY2(t[i]);
-        //std::cout << y1[i] << " " << testy1 << " " << y2[i] << " " << testy2 << " " << y1[i] - testy1 << " " << y2[i] - testy2 << "\n";
         std::cout << y[i][0] << " " << testy1 << " " << y[i][1] << " " << testy2 << " " << y[i][0]-testy1<<" "<< y[i][1]-testy2 <<"\n";
     }
-    */
 
 }
